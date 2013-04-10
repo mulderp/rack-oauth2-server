7216d3d63a4fdfd7bd62136f32be766c6149b018
Patrick Mulder <mulder.patrick@gmail.com>
2013-04-10 23:33:11 +0200

    extract rails

--- rack-oauth2-rails/lib/rack-oauth2-server.rb
+++ rack-oauth2-rails/lib/rack-oauth2-server.rb
@@ -1,4 +1,3 @@
 require "rack/oauth2/server"
-require "rack/oauth2/sinatra" if defined?(Sinatra)
 require "rack/oauth2/rails" if defined?(Rails)
 require "rack/oauth2/server/railtie" if defined?(Rails::Railtie)
--- rack-oauth2-rails/test/setup.rb
+++ rack-oauth2-rails/test/setup.rb
@@ -16,7 +16,7 @@ require "rack/oauth2/server/admin"
 ENV["RACK_ENV"] = "test"
 ENV["DB"] = "rack_oauth2_server_test"
 DATABASE = Mongo::Connection.new[ENV["DB"]]
-FRAMEWORK = ENV["FRAMEWORK"] || "sinatra"
+FRAMEWORK = "rails"
 
 
 $logger = Logger.new("test.log")
@@ -31,73 +31,46 @@ Rack::OAuth2::Server::Admin.configure do |config|
 end
 
 
-case FRAMEWORK
-when "sinatra", nil
+RAILS_ENV = "test"
+RAILS_ROOT = File.dirname(__FILE__) + "/rails3"
+begin
+  require "rails"
+rescue LoadError
+end
 
-  require "sinatra/base"
-  puts "Testing with Sinatra #{Sinatra::VERSION}"
-  require File.dirname(__FILE__) + "/sinatra/my_app"
-  
+if defined?(Rails::Railtie)
+  # Rails 3.x
+  require "rack/oauth2/server/railtie"
+  require File.dirname(__FILE__) + "/rails3/config/environment"
+  puts "Testing with Rails #{Rails.version}"
+
   class Test::Unit::TestCase
     def app
-      Rack::Builder.new do
-        map("/oauth/admin") { run Server::Admin }
-        map("/") { run MyApp }
-      end
+      ::Rails.application
     end
 
     def config
-      MyApp.oauth
-    end
-  end
-
-when "rails"
-
-  RAILS_ENV = "test"
-  RAILS_ROOT = File.dirname(__FILE__) + "/rails3"
-  begin
-    require "rails"
-  rescue LoadError
-  end
-
-  if defined?(Rails::Railtie)
-    # Rails 3.x
-    require "rack/oauth2/server/railtie"
-    require File.dirname(__FILE__) + "/rails3/config/environment"
-    puts "Testing with Rails #{Rails.version}"
-  
-    class Test::Unit::TestCase
-      def app
-        ::Rails.application
-      end
-
-      def config
-        ::Rails.configuration.oauth
-      end
-    end
-
-  else
-    # Rails 2.x
-    RAILS_ROOT = File.dirname(__FILE__) + "/rails2"
-    require "initializer"
-    require "action_controller"
-    require File.dirname(__FILE__) + "/rails2/config/environment"
-    puts "Testing with Rails #{Rails.version}"
-  
-    class Test::Unit::TestCase
-      def app
-        ActionController::Dispatcher.new
-      end
-
-      def config
-        ::Rails.configuration.oauth
-      end
+      ::Rails.configuration.oauth
     end
   end
 
 else
-  puts "Unknown framework #{FRAMEWORK}"
-  exit -1
+  # Rails 2.x
+  RAILS_ROOT = File.dirname(__FILE__) + "/rails2"
+  require "initializer"
+  require "action_controller"
+  require File.dirname(__FILE__) + "/rails2/config/environment"
+  puts "Testing with Rails #{Rails.version}"
+
+  class Test::Unit::TestCase
+    def app
+      ActionController::Dispatcher.new
+    end
+
+    def config
+      ::Rails.configuration.oauth
+    end
+  end
 end
 
 
